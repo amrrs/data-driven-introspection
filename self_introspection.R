10a361a57c2d9be4f1f449d9b03c0aa75b5e4530
amrrs <amrrs@outlook.com>
2019-02-12 22:37:35 +0530

    license

--- self_introspection.R
+++ self_introspection.R
@@ -237,4 +237,7 @@ me_count %>%
     x = "App name"
   ) -> y3
 
-cowplot::plot_grid(y1,y2,y3, ncol = 3, scale = 0.7)
+cowplot::plot_grid(y1,y2,y3, ncol = 3, scale = 0.7, vjust = 0, label_size = 8)
+
+
+#usethis::use_mit_license("Github @amrrs")
